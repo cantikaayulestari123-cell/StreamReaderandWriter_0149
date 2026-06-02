4c246f617c6d379a5821e4a16e112058d7be5b76
cantikaayulestari123-cell <cantikaayulestari123@gmail.com>
2026-06-02 18:02:37 +0700

    kondisi jika throw yang bertipe data integer

--- manualexception.cpp
+++ manualexception.cpp
@@ -8,5 +8,8 @@ int main()
         cout << "Selamat belajar di prodi TI UMY" << endl;
          //throw 0.5; //melemparkan sebuah integer maka
         cout << "Pernyataan tidak akan ditampilkan" << endl;
-    
     }
+        //kondisi jika throw yang bertipe data integer
+        catch (int a){
+            
+        }
