4769d5c6058e141eebfa7ff3a3a1ad1db26637bd
cantikaayulestari123-cell <cantikaayulestari123@gmail.com>
2026-06-02 18:40:29 +0700

    menutup file apabila telah selesai di tulis

--- iofilewithpath.cpp
+++ iofilewithpath.cpp
@@ -30,6 +30,10 @@ int main()
         if (baris == "q") break;
         //menulis dan memasukkan nilai dari baris ke dalam file
         outfile << baris << endl;
-
     }
+    //menutup file apabila telah selesai di tulis
+    outfile.close();
+
+    
+
 }
